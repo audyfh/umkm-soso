6cce58ce063481a4c050a7738ba29b42952b3182
audyfh <155510215+audyfh@users.noreply.github.com>
2025-07-26 19:04:55 +0700

    final

--- script.js
+++ script.js
@@ -4,15 +4,16 @@ fetch("umkm.json")
     const container = document.getElementById("umkm-container");
     data.forEach((item) => {
       const card = document.createElement("div");
-      card.className = "w-full max-w-md border-b-2 pb-4 border-black md:border-none";
+      card.className =
+        "w-full max-w-md border-b pb-4 border-black md:border-none";
       card.innerHTML = `
   <div class="flex gap-3 md:flex-col md:gap-6">
     <img src="${item.productImage[0]}" alt="${item.name}" class="w-32 h-32 rounded-md object-cover md:w-full md:h-48" />
     <div class="text-start justify-between flex flex-col py-2 md:py-0">
-      <h2 class="text-base font-semibold md:text-lg">${item.name}</h2>
+      <h2 class="text-base font-bold md:text-lg text-blue-600">${item.name}</h2>
       <p class="text-xs text-gray-600 line-clamp-2 md:text-sm">${item.short_desc}</p>
       <div class="mt-2 md:mt-4">
-        <button class="lihat-button bg-blue-600 hover:bg-blue-700 text-white text-xs md:text-sm px-3 py-1 rounded-full w-32 md:w-full transition">
+        <button class="lihat-button bg-blue-500 hover:bg-blue-600 text-white text-xs md:text-sm px-3 py-2 rounded-full w-32 md:w-full transition">
           Lihat Selengkapnya
         </button>
       </div>
@@ -46,8 +47,24 @@ function showPopup(data) {
   document.getElementById("popup-owner").textContent = owner || "";
   document.getElementById("popup-description").textContent = description;
   document.getElementById("wa-link").href = waLink;
-  document.getElementById("shopee-link").href = shopeeLink;
   document.getElementById("popup-map").innerHTML = location;
+  const shopeeBtn = document.getElementById("shopee-link");
+  if (shopeeLink && shopeeLink.trim() !== "") {
+    shopeeBtn.href = shopeeLink;
+    shopeeBtn.classList.remove("hidden");
+    if (shopeeLink.includes("tokopedia.com")) {
+      shopeeBtn.textContent = "Link Tokopedia";
+      shopeeBtn.classList.remove("bg-orange-500");
+      shopeeBtn.classList.add("bg-green-600");
+    } else {
+      shopeeBtn.textContent = "Link Shopee";
+      shopeeBtn.href = shopeeLink;
+      shopeeBtn.classList.remove("bg-green-600");
+      shopeeBtn.classList.add("bg-orange-500");
+    }
+  } else {
+    shopeeBtn.classList.add("hidden");
+  }
 
   // Swiper Produk
   const swiperMain = document.getElementById("popup-main-swiper");
